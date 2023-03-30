0aa50ce015a2dd9f80b3d6e1c78ba14912d1f289
beligrous <beligrous@gmail.com>
2023-03-30 11:30:36 +0300

    add createPostScreen

--- Screens/Main/PostsScreen.js
+++ Screens/Main/PostsScreen.js
@@ -1,19 +1,44 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState, useEffect } from "react";
+import { Image, View, StyleSheet, FlatList } from "react-native";
 
-const PostsScreen = () => {
+const PostsScreen = ({ route }) => {
+  const [posts, setPosts] = useState([]);
+  console.log(route);
+
+  useEffect(() => {
+    if (!route.params) {
+      return;
+    }
+    setPosts((prevState) => {
+      [...prevState, route.params];
+    });
+  }, [route.params]);
+
   return (
     <View style={styles.container}>
-      <Text>PostScreen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, i) => {
+          i.toString();
+        }}
+        renderItem={({ item }) => (
+          <View>
+            <Image source={{ uri: item.photo }} style={styles.image} />
+          </View>
+        )}
+      />
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+  },
+  image: {
+    marginHorizontal: 16,
+    height: 240,
   },
 });
 
